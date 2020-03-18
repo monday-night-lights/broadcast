15995642f8ab8239c1540264c9aa7d33a502dc88
Jeremy Drager <jdrager2@gmail.com>
2020-03-18 01:49:55 -0400

    Fix UI

--- src/app/keeper/keeper.component.ts
+++ src/app/keeper/keeper.component.ts
@@ -58,21 +58,17 @@ export class KeeperComponent implements OnInit {
   }
 
   public updateScore(scores: any) {
-    console.log(scores);
     let value = scores.value;
     let score = new Score();
     score.period = value["period"];
-    score.homeTeam = new Team(value["homeTeam-name"], value["homeTeam-logo"], value["homeTeam-color"], value["homeTeam-gamesWon"], value["homeTeam-score"]);
-    score.awayTeam = new Team(value["awayTeam-name"], value["awayTeam-logo"], value["awayTeam-color"], value["awayTeam-gamesWon"], value["awayTeam-score"]);
+    score.homeTeam = new Team(value["homeTeam-name"], value["homeTeam-logo"], value["homeTeam-color"], value["homeTeam-gamesWon"], value["homeTeam-score"], true);
+    score.awayTeam = new Team(value["awayTeam-name"], value["awayTeam-logo"], value["awayTeam-color"], value["awayTeam-gamesWon"], value["awayTeam-score"], false);
     score.playByPlay = new Announcer(value["playByPlay-announcerName"], value["playByPlay-title"], value["playByPlay-subtitle"]);
     score.colorCommentary = new Announcer(value["colorCommentary-announcerName"], value["colorCommentary-title"], value["colorCommentary-subtitle"]);
     score.fieldReporter = new Announcer(value["fieldReporter-announcerName"], value["fieldReporter-title"], value["fieldReporter-subtitle"]);
 
-    console.log(score);
     this.fileService.saveScore(score).subscribe(val => {
-
       console.log(val);
-
     });
   }
 }
--- src/app/team/team.ts
+++ src/app/team/team.ts
@@ -6,11 +6,14 @@ export class Team {
     gamesWon: number;
     score: number;
 
-    constructor (name: string, logo: string, color: string, gamesWon: number, score: number) {
+    isHomeTeam: boolean;
+
+    constructor (name: string, logo: string, color: string, gamesWon: number, score: number, isHomeTeam: boolean) {
         this.teamName = name;
         this.logo = logo;
         this.color = color;
         this.gamesWon = gamesWon;
         this.score = score;
+        this.isHomeTeam = isHomeTeam;
     }
 }
