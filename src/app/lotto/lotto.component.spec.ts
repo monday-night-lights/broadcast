aa19f02fdd4f635c51858153786bfd0d17173605
Jeremy Drager <jdrager2@gmail.com>
2023-02-28 23:44:02 -0500

    update

--- src/app/lotto/lotto.component.spec.ts
+++ src/app/lotto/lotto.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LottoComponent } from './lotto.component';
 
@@ -6,7 +6,7 @@ describe('LottoComponent', () => {
   let component: LottoComponent;
   let fixture: ComponentFixture<LottoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LottoComponent ]
     })
