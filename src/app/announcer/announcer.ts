4349a2212a12e4d4cf9cae7d8f2cd415a0051312
Jeremy Drager <jdrager2@gmail.com>
2026-03-17 13:08:50 -0400

    Rename order to screenOrder

--- src/app/announcer/announcer.ts
+++ src/app/announcer/announcer.ts
@@ -2,12 +2,12 @@ export class Announcer {
     announcerName: string;
     title: string;
     subtitle: string;
-    order: any;
+    screenOrder: any;
     
-    constructor(name: string, title: string, subtitle: string, order: number) {
+    constructor(name: string, title: string, subtitle: string, screenOrder: number) {
         this.announcerName = name;
         this.title = title;
         this.subtitle = subtitle;
-        this.order = order;
+        this.screenOrder = screenOrder;
     }
 }
